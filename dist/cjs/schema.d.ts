4c749f6c9f065c96a3e1ca779271aa92f00302d6
Roman Kirsanov <kirsanov2008@gmail.com>
2022-05-04 18:26:07 -0500

    .

--- dist/cjs/schema.d.ts
+++ dist/cjs/schema.d.ts
@@ -17,8 +17,8 @@ export declare const ifEmpty: (value: any, value2: string) => string;
 export declare const isCyclic: (value: any) => boolean;
 export declare const isDeepEqual: (value: any, value2: any) => boolean;
 export declare type SchemaBase<T> = {
-    readonly optional: (T extends any ? (boolean | undefined) : T extends undefined ? true : false);
-    readonly nullable: (T extends any ? (boolean | undefined) : T extends null ? true : false);
+    readonly optional?: boolean;
+    readonly nullable?: boolean;
     readonly fallback?: (T | null);
     readonly equal?: T;
     readonly oneOf?: T[];
